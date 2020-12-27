c37adc4a4718eb96fd078deda1678abdfea57e9d
Peng Wang <45309241+wangpengnorman@users.noreply.github.com>
2020-12-27 15:43:20 +0800

    Update data.py

--- tools/data.py
+++ tools/data.py
@@ -32,7 +32,7 @@ class data(Dataset):
         for x in Data:
             jpg_name.append(os.path.join(jpg_path, x['image'])+'.jpg')
             jpg_label.append(x['label'])
-            f_label.append(0)#x['f_label'])
+            f_label.append(0)
             expression.append(x['expression'])
             bbox.append(x['bbox'])
             facts.append(x['facts'])
@@ -69,22 +69,34 @@ class data(Dataset):
             locations.append([bbox[0] / self.w_h[item][0], bbox[1] /self.w_h[item][1], (bbox[0]+bbox[2]) / self.w_h[item][0], (bbox[1]+bbox[3]) / self.w_h[item][1], bbox[2]*bbox[3] / self.w_h[item][0] /self.w_h[item][1]])
             if self.transform is not None:
                 can = self.transform(can)
+            #print(np.array(can).shape)
             local.append(np.array(can))
         local = torch.from_numpy(np.array(local)).view(-1, 224, 224)
         expression = self.expression[item][:self.e_mask[item]]
-        expression = np.pad(np.array(expression), (0, 50-self.e_mask[item]), 'constant', constant_values=(0, 9999))
+        #print(expression, type(expression))
+        random.shuffle(expression)
+        #print(np.array(expression))
+        expression = np.pad(np.array(expression), (0, 50-self.e_mask[item]), 'constant', constant_values=(0, 15732))
         expression = torch.from_numpy(np.array(expression)).type(torch.LongTensor)
+        #print(np.array(self.facts[item]).shape)
         facts = torch.from_numpy(np.array(self.facts[item])).type(torch.LongTensor).view(-1, 50)
         locations = torch.from_numpy(np.array(locations)).type(torch.FloatTensor)
+        #print(self.mask[item])
+        #print(np.array(self.mask[item]).shape)
         mask = []
+        #print(len(self.mask[item][0]))
+        #print(len(self.mask[item]))
         f_mask = []
         ff_mask = []
         for i in range(len(self.mask[item])):
             middle = []
             l = 0
-            for j in range(len(self.mask[item][0])):
+            #print(len(self.mask[item][i]))
+            #print(self.mask[item][i])
+            for j in range(len(self.mask[item][0])):#len(self.mask[item][i])):
                 if self.mask[item][i][j] > 0:
-                    middle.append(list(np.pad(np.ones(self.mask[item][i][j]), (0, 50-self.mask[item][i][j]))))# / self.mask[item][i][j]))
+                    middle.append(list(np.pad(np.ones(self.mask[item][i][j]), (0, 50-self.mask[item][i][j])) / self.mask[item][i][j]))
+                    #middle.append(list(np.eye(50)[self.mask[item][i][j]-1]))
                     l += 1
                 else:
                     middle.append(list(np.zeros(50)))
@@ -92,14 +104,18 @@ class data(Dataset):
             f_mask.append(list(np.pad(np.ones(l), (0, 50-l), 'constant')))
             ff_mask.append(list(np.eye(50)[l-1]))
         mask = np.array(mask)
+        #print(mask.shape)
         mask = torch.from_numpy(mask).type(torch.FloatTensor)
         f_mask = np.array(f_mask)
         f_mask = torch.from_numpy(f_mask).type(torch.FloatTensor)
         ff_mask = np.array(ff_mask)
         ff_mask = torch.from_numpy(ff_mask).type(torch.FloatTensor)
-        e_mask = np.pad(np.ones(self.e_mask[item]), (0, 50-self.e_mask[item]))
+        #mask = torch.from_numpy(np.array(self.mask[item])).type(torch.FloatTensor)
+        #e_mask = np.eye(50)[self.e_mask[item]-1]
+        e_mask = np.pad(np.ones(self.e_mask[item]), (0, 50-self.e_mask[item]))# / self.e_mask[item]
         e_mask = torch.from_numpy(e_mask).type(torch.FloatTensor)
-        c_mask = np.pad(np.ones(self.c_mask[item]), (0, 10-self.c_mask[item]))
+        #e_mask = torch.from_numpy(np.array(self.e_mask[item])).type(torch.FloatTensor)
+        c_mask = np.pad(np.ones(self.c_mask[item]), (0, 10-self.c_mask[item]))# / self.e_mask[item]
         c_mask = torch.from_numpy(c_mask).type(torch.FloatTensor)
         return jpg, label, f_label, expression, e_mask, local, locations, facts, mask, f_mask, ff_mask, jpg_name, c_mask
 
@@ -113,7 +129,3 @@ if __name__ == '__main__':
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
-    a = data('./image','./json/N_A_test.json', data_transform=test_transform)
-    jpg, label, f_label, expression, e_mask, local, locations, facts, mask, jpg_name = a.__getitem__(0)
-    print(expression, len(expression))
-    print(e_mask)
